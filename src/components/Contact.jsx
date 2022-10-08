1ed06263c49103501e12b628660465e4f022c095
Tomas Dangelo <tomasdangelo7@outlook.es>
2022-10-08 13:07:35 -0300

    Agregando Weather app

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen border-t-4 bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/29bc31bc-d35b-4fb1-ac83-7748d08665e0" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contacto</p>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,17 +5,17 @@ import Toms from '../assets/logo-toms.png'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pb-44'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
+        <div className='pb-4'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Mis trabajos
           </p>
-          <p className='py-6'>Estos son algunos de los proyectos que realicé. Aguamerica fue mi primer proyecto, el cual hice para el curso de Desarrollo Web. Ruth Pastelería mi segundo proyecto, y está hecho con JavaScript. TOM'S Store fue el tercero; está hecho con ReactJS. Si bien el diseño es mejorable en todos mis proyectos, lo central de los cursos fue la funcionalidad. Además, en este momento estoy trabajando en nuevos proyectos con ReactJS para adquirir experiencia. </p>
+          <p className='py-6'>Estos son algunos de los proyectos que realicé. Aguamerica fue mi primer proyecto, el cual hice para el curso de Desarrollo Web. Ruth Pastelería mi segundo proyecto, y está hecho con JavaScript. TOM'S Store fue el tercero; está hecho con ReactJS. Weather App es el cuarto; es un proyecto hecho con ReactJS, que obtiene datos del clima a través de una API para mostrarlo a los usuarios. Si bien el diseño es mejorable en todos mis proyectos, lo central de los cursos fue la funcionalidad. Además, en este momento estoy trabajando en nuevos proyectos con ReactJS para adquirir experiencia. </p>
         </div>
 
 {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        <div className='grid sm:grid-cols-2  gap-4'>
 
             {/* Grid Item */}
           <div style={{ backgroundImage: `url(${Aguamerica})` }}className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
@@ -81,7 +81,31 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>      
+          </div>
+          <div
+            style={{ backgroundImage: `url(https://i0.wp.com/enlacelatinonc.org/wp-content/uploads/2022/04/El-Clima-hoy.jpg?fit=1200%2C676&ssl=1)` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-black tracking-wider'>
+              </span>
+              <div className='pt-8 text-center'>
+                <a target={"blank"} href='https://tomasdangelo-weather-app.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a target={"blank"} href='https://github.com/TomasDangelo/Tomasdangelo-Weather-App'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Código
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </div>
