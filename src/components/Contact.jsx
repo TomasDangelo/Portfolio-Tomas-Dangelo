7eb2b31b67fad07ae66fefb4c95713d83c3f81cc
TomasDangelo <104146883+TomasDangelo@users.noreply.github.com>
2025-01-27 11:26:10 -0800

    buttons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <a className='text-pink-600 inline font-bold mr-2' href="mailto:tomasdangelo7@outlook.es">Enviame un email</a> 
-        <a className='text-pink-600 inline font-bold ml-2' href="https://wa.me/543415987640?text=Hola,%20vi%20tu%20portfolio%20y%20quiero%20contactarte." target="_blank" rel="noopener noreferrer"
->
-  Contáctame por WhatsApp
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+        <a className='text-pink-600 inline font-bold mr-2' href="mailto:tomasdangelo7@outlook.es">Enviame un email</a>               
+        </button>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>      
+        <a className='text-pink-600 inline font-bold ml-2' href="https://wa.me/543415987640?text=Hola,%20vi%20tu%20portfolio%20y%20quiero%20contactarte." target="_blank" rel="noopener noreferrer"> Contáctame por WhatsApp
 </a>
+</button>
+
 
     </div>
   )
